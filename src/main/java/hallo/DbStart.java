82b18f784a28ea58a95e8b83e1256f99cad3cefb
Piotr Marecki <pmarecki.pro@gmail.com>
2016-10-15 09:31:28 +0200

    Podłączenie bazy h2 na pliku nowabaza

---
+++ src/main/java/hallo/DbStart.java
@@ -0,0 +1,26 @@
+package hallo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created on 15.10.16, at 07:29
+ */
+public class DbStart {
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/nowabaza", "sa", "");
+
+        PreparedStatement st = conn.prepareStatement("select * from abc");
+        ResultSet rs = st.executeQuery();
+        while (rs.next()) {
+            String name = rs.getString("nazwa");
+            System.out.println(name);
+        }
+        // add application code here
+        conn.close();
+
+    }
+}
